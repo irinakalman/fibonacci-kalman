72957ad774e0e3af7027609d6171e48e770bb001
irinakalman <81618370+irinakalman@users.noreply.github.com>
2023-04-17 00:30:21 +0300

    fibonacci formula

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('fibo');
+
+function fibonacci(n){
+    if(n==0 || n == 1) return n; 
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+function main(){
+    console.log(fibonacci(10));
+}
+
+main();
